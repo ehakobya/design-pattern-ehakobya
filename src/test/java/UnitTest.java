dccf33f0e48e2ad4fb9f83bc49a094541c4017d8
ehakobya <ehakobya@asu.edu>
2023-04-14 10:10:27 -0700

    Initial setup

---
+++ src/test/java/UnitTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+
+public class UnitTest {
+
+    @org.junit.Before
+    public void setUp() {
+    }
+
+    @Test
+    public void test1() {
+        assertEquals(1, 1);
+    }
+}
