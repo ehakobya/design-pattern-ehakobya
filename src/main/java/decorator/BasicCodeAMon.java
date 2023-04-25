24297ed8bc8bfce7e1fc6ad2a232ece1d9eaef32
ehakobya <ehakobya@asu.edu>
2023-04-24 22:28:05 -0700

    Remove decorator pattern
    CodeAMon generation was refactored into the factory package and it is implemented with the factory pattern

--- src/main/java/decorator/BasicCodeAMon.java
+++ src/main/java/decorator/BasicCodeAMon.java
@@ -1,140 +1,142 @@
-package decorator;
-
-public class BasicCodeAMon implements CodeAMon {
-
-	double currentHP = 100;
-	String name;
-	double maxHP = 100;
-	double xp = 0;
-	double levelUpThreshold = 20;
-	int level = 1;
-	double attack = 40;
-	double bonus = 0;
-	boolean isDead = false;
-	String type = "Basic";
-
-	@Override
-	public void attack(CodeAMon opponent) {
-		if (!this.isDead() && !opponent.isDead()) {
-			if (this.getAttack() > opponent.getCurrentHP()) {
-				opponent.setIsDead(true);
-			} else {
-				opponent.setCurrentHP(this.getAttack());
-				opponent.addXP(this.getAttack());
-			}
-		}
-	}
-
-	@Override
-	public void takeDamage(CodeAMon opponent) {
-		if (!this.isDead() && !opponent.isDead()) {
-			if (opponent.getAttack() > this.getCurrentHP()) {
-				this.setIsDead(true);
-			} else {
-				this.setCurrentHP(opponent.getAttack());
-				this.addXP(opponent.getAttack() / 2);
-			}
-		}
-	}
-
-	@Override
-	public double heal() {
-		double healAmount = 0;
-		if (!this.isDead() && currentHP <= maxHP / 2) {
-			double previousHP = currentHP;
-			// if health below 50% then heal by 20%
-			currentHP += currentHP * 0.2;
-			healAmount = currentHP - previousHP;
-		}
-		return healAmount;
-	}
-
-	@Override
-	public void levelUp() {
-		if (!this.isDead() && this.xp >= levelUpThreshold) {
-			levelUpThreshold += levelUpThreshold * 0.2; // increase threshold by 20%
-			xp = 0; // reset xp
-			level++; // increase level
-		}
-	}
-
-	@Override
-	public double getCurrentHP() {
-		return this.currentHP;
-	}
-
-	@Override
-	public String getName() {
-		return this.name;
-	}
-
-	@Override
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	@Override
-	public double getMaxHP() {
-		return this.maxHP;
-	}
-
-	@Override
-	public double getXp() {
-		return this.xp;
-	}
-
-	@Override
-	public double getAttack() {
-		return this.attack + (this.attack * bonus / 100);
-	}
-
-	@Override
-	public int getLevel() {
-		return this.level;
-	}
-
-	@Override
-	public double getLevelUpThreshold() {
-		return this.levelUpThreshold;
-	}
-
-	@Override
-	public double getBonus() {
-		return this.bonus;
-	}
-
-	@Override
-	public boolean isDead() {
-		return !(currentHP > 0);
-	}
-
-	@Override
-	public String getType() {
-		return this.type;
-	}
-
-	@Override
-	public void setIsDead(Boolean bool) {
-		this.isDead = true;
-	}
-
-	@Override
-	public void setCurrentHP(double hp) {
-		this.currentHP = hp;
-	}
-
-	@Override
-	public void addXP(double xp) {
-		this.xp += xp;
-	}
-
-	@Override
-	public void setType(String type) {
-		this.type = type;
-	}
-
-	@Override
-	public void setBonus(double bonus) {
-		this.bonus = bonus;
-	}
-}
+//package decorator;
+//
+//import factory.CodeAMon;
+//
+//public class BasicCodeAMon implements CodeAMon {
+//
+//	double currentHP = 100;
+//	String name;
+//	double maxHP = 100;
+//	double xp = 0;
+//	double levelUpThreshold = 20;
+//	int level = 1;
+//	double attack = 40;
+//	double bonus = 0;
+//	boolean isDead = false;
+//	String type = "Basic";
+//
+//	@Override
+//	public void attack(CodeAMon opponent) {
+//		if (!this.getIsAlive() && !opponent.getIsAlive()) {
+//			if (this.getAttack() > opponent.getHP()) {
+//				opponent.setIsAlive(true);
+//			} else {
+//				opponent.setHP(this.getAttack());
+//				opponent.setXP(this.getAttack());
+//			}
+//		}
+//	}
+//
+//	@Override
+//	public void takeDamage(CodeAMon opponent) {
+//		if (!this.getIsAlive() && !opponent.getIsAlive()) {
+//			if (opponent.getAttack() > this.getHP()) {
+//				this.setIsAlive(true);
+//			} else {
+//				this.setHP(opponent.getAttack());
+//				this.setXP(opponent.getAttack() / 2);
+//			}
+//		}
+//	}
+//
+//	@Override
+//	public double heal() {
+//		double healAmount = 0;
+//		if (!this.getIsAlive() && currentHP <= maxHP / 2) {
+//			double previousHP = currentHP;
+//			// if health below 50% then heal by 20%
+//			currentHP += currentHP * 0.2;
+//			healAmount = currentHP - previousHP;
+//		}
+//		return healAmount;
+//	}
+//
+//	@Override
+//	public void levelUp() {
+//		if (!this.getIsAlive() && this.xp >= levelUpThreshold) {
+//			levelUpThreshold += levelUpThreshold * 0.2; // increase threshold by 20%
+//			xp = 0; // reset xp
+//			level++; // increase level
+//		}
+//	}
+//
+//	@Override
+//	public double getHP() {
+//		return this.currentHP;
+//	}
+//
+//	@Override
+//	public String getName() {
+//		return this.name;
+//	}
+//
+//	@Override
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//
+//	@Override
+//	public double getMaxHP() {
+//		return this.maxHP;
+//	}
+//
+//	@Override
+//	public double getXp() {
+//		return this.xp;
+//	}
+//
+//	@Override
+//	public double getAttack() {
+//		return this.attack + (this.attack * bonus / 100);
+//	}
+//
+//	@Override
+//	public int getLevel() {
+//		return this.level;
+//	}
+//
+//	@Override
+//	public double getLevelUpThreshold() {
+//		return this.levelUpThreshold;
+//	}
+//
+//	@Override
+//	public double getBonus() {
+//		return this.bonus;
+//	}
+//
+//	@Override
+//	public boolean getIsAlive() {
+//		return !(currentHP > 0);
+//	}
+//
+//	@Override
+//	public String getType() {
+//		return this.type;
+//	}
+//
+//	@Override
+//	public void setIsAlive(Boolean bool) {
+//		this.isDead = true;
+//	}
+//
+//	@Override
+//	public void setHP(double hp) {
+//		this.currentHP = hp;
+//	}
+//
+//	@Override
+//	public void setXP(double xp) {
+//		this.xp += xp;
+//	}
+//
+//	@Override
+//	public void setType(String type) {
+//		this.type = type;
+//	}
+//
+//	@Override
+//	public void setBonus(double bonus) {
+//		this.bonus = bonus;
+//	}
+//}
--- src/main/java/decorator/CodeAMonDecorator.java
+++ src/main/java/decorator/CodeAMonDecorator.java
@@ -1,75 +1,77 @@
-package decorator;
-
-public class CodeAMonDecorator implements CodeAMon {
-
-	protected CodeAMon codeAMon;
-
-	public CodeAMonDecorator(CodeAMon c) {
-		this.codeAMon = c;
-	}
-
-	@Override
-	public void attack(CodeAMon opponent) { this.codeAMon.attack(opponent);}
-
-	@Override
-	public void takeDamage(CodeAMon opponent) { this.codeAMon.takeDamage(opponent); }
-
-	@Override
-	public double heal() { return this.codeAMon.heal(); }
-
-	@Override
-	public void levelUp() { this.codeAMon.levelUp(); }
-
-	@Override
-	public double getCurrentHP() { return this.codeAMon.getCurrentHP(); }
-
-	@Override
-	public String getName() {
-		return this.codeAMon.getName();
-	}
-
-	@Override
-	public void setName(String name) {
-		this.codeAMon.setName(name);
-	}
-
-	@Override
-	public double getMaxHP() { return this.codeAMon.getMaxHP(); }
-
-	@Override
-	public double getXp() { return this.codeAMon.getXp(); }
-
-	@Override
-	public double getAttack() { return this.codeAMon.getAttack(); }
-
-	@Override
-	public int getLevel() { return this.codeAMon.getLevel(); }
-
-	@Override
-	public double getLevelUpThreshold() { return this.codeAMon.getLevelUpThreshold(); }
-
-	@Override
-	public double getBonus() { return this.codeAMon.getBonus(); }
-
-	@Override
-	public boolean isDead() { return this.codeAMon.isDead(); }
-
-	@Override
-	public String getType() { return this.codeAMon.getType(); }
-
-	@Override
-	public void setIsDead(Boolean bool) { this.codeAMon.setIsDead(bool); }
-
-	@Override
-	public void setCurrentHP(double hp) { this.codeAMon.setCurrentHP(hp); }
-
-	@Override
-	public void addXP(double xp) { this.codeAMon.addXP(xp); }
-
-	@Override
-	public void setType(String type) { this.codeAMon.setType(type); }
-
-	@Override
-	public void setBonus(double bonus) { this.codeAMon.setBonus(bonus);
-	}
-}
+//package decorator;
+//
+//import factory.CodeAMon;
+//
+//public class CodeAMonDecorator implements CodeAMon {
+//
+//	protected CodeAMon codeAMon;
+//
+//	public CodeAMonDecorator(CodeAMon c) {
+//		this.codeAMon = c;
+//	}
+//
+//	@Override
+//	public void attack(CodeAMon opponent) { this.codeAMon.attack(opponent);}
+//
+//	@Override
+//	public void takeDamage(CodeAMon opponent) { this.codeAMon.takeDamage(opponent); }
+//
+//	@Override
+//	public double heal() { return this.codeAMon.heal(); }
+//
+//	@Override
+//	public void levelUp() { this.codeAMon.levelUp(); }
+//
+//	@Override
+//	public double getHP() { return this.codeAMon.getHP(); }
+//
+//	@Override
+//	public String getName() {
+//		return this.codeAMon.getName();
+//	}
+//
+//	@Override
+//	public void setName(String name) {
+//		this.codeAMon.setName(name);
+//	}
+//
+//	@Override
+//	public double getMaxHP() { return this.codeAMon.getMaxHP(); }
+//
+//	@Override
+//	public double getXp() { return this.codeAMon.getXp(); }
+//
+//	@Override
+//	public double getAttack() { return this.codeAMon.getAttack(); }
+//
+//	@Override
+//	public int getLevel() { return this.codeAMon.getLevel(); }
+//
+//	@Override
+//	public double getLevelUpThreshold() { return this.codeAMon.getLevelUpThreshold(); }
+//
+//	@Override
+//	public double getBonus() { return this.codeAMon.getBonus(); }
+//
+//	@Override
+//	public boolean getIsAlive() { return this.codeAMon.getIsAlive(); }
+//
+//	@Override
+//	public String getType() { return this.codeAMon.getType(); }
+//
+//	@Override
+//	public void setIsAlive(Boolean bool) { this.codeAMon.setIsAlive(bool); }
+//
+//	@Override
+//	public void setHP(double hp) { this.codeAMon.setHP(hp); }
+//
+//	@Override
+//	public void setXP(double xp) { this.codeAMon.setXP(xp); }
+//
+//	@Override
+//	public void setType(String type) { this.codeAMon.setType(type); }
+//
+//	@Override
+//	public void setBonus(double bonus) { this.codeAMon.setBonus(bonus);
+//	}
+//}
