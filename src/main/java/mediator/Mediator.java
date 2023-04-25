f51645a3d4b39eeafe5534f32c0c8d38959cf522
ehakobya <ehakobya@asu.edu>
2023-04-25 01:30:56 -0700

    Update Mediator.java

--- src/main/java/mediator/Mediator.java
+++ src/main/java/mediator/Mediator.java
@@ -1,25 +1,25 @@
 package mediator;
 
 import builder.Trainer;
-import decorator.CodeAMon;
+import factory.CodeAMon;
 
 import java.util.ArrayList;
 
 public interface Mediator {
 
-	public ArrayList<Trainer> getTrainers();
+    public ArrayList<Trainer> getTrainers();
 
-	public void registerTrainer(Trainer t);
+    public void registerTrainer(Trainer t);
 
-	public void unregisterTrainer(Trainer t);
+    public void unregisterTrainer(Trainer t);
 
-	public void heal(ArrayList<Trainer> trainers);
+    public void heal(ArrayList<Trainer> trainers);
 
-	public void beginBattle(Trainer t1, Trainer t2);
+    public void battleInit(Trainer t1, Trainer t2);
 
-	public void endBattle(Trainer t1, Trainer t2);
+    public void battleFinish(Trainer t1, Trainer t2);
 
-	public void changeWeather();
+    public void changeWeather();
 
-	public void attack(CodeAMon c1, CodeAMon c2);
+    public void attack(CodeAMon c1, CodeAMon c2);
 }
