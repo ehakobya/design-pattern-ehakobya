f94f6dd76aae29b07d79943b6afd1d5e4f7e7cdf
ehakobya <ehakobya@asu.edu>
2023-04-25 01:30:48 -0700

    Update FireMon.java

--- src/main/java/factory/FireMon.java
+++ src/main/java/factory/FireMon.java
@@ -1,77 +1,86 @@
 package factory;
 
-import factory.CodeAMon;
-
 import java.util.Random;
 
 public class FireMon extends CodeAMon {
 
-	Random rnd = new Random();
-	private String[] codeAMonNameBucket = {
-			"Pikachu",
-			"Charmander",
-			"Squirtle",
-			"Bulbasaur",
-			"Eevee",
-			"Jigglypuff",
-			"Snorlax",
-			"Mewtwo",
-			"Dragonite",
-			"Gyarados",
-			"Lapras",
-			"Magikarp",
-			"Vaporeon",
-			"Flareon",
-			"Jolteon",
-			"Articuno",
-			"Zapdos",
-			"Moltres",
-			"Mew",
-			"Ditto"
-	};
+    Random rnd = new Random();
+    private String[] codeAMonNameBucket = {
+            "Pikachu",
+            "Charmander",
+            "Squirtle",
+            "Bulbasaur",
+            "Eevee",
+            "Jigglypuff",
+            "Snorlax",
+            "Mewtwo",
+            "Dragonite",
+            "Gyarados",
+            "Lapras",
+            "Magikarp",
+            "Vaporeon",
+            "Flareon",
+            "Jolteon",
+            "Articuno",
+            "Zapdos",
+            "Moltres",
+            "Mew",
+            "Ditto"
+    };
 
-	public FireMon() {
-		setType("FIRE");
-		setAttack(rnd.nextInt(6) + 15); // generate random attack from 15 to 20
-		setAbility("FIRE BALL", 25);
-		setName(codeAMonNameBucket[rnd.nextInt(21)]); // select random name from 0 to 20
-//		System.out.println("\n>>> DEBUG: Created basic code-a-mon from FireMon\n");
-	}
+    /**
+     * Constructor for the FireMon class, which initializes
+     * a new FireMon object with a predetermined attack, ability, type,
+     * and randomly selected name.
+     */
+    public FireMon() {
+        setType("FIRE");
+        setAttack(25); // generate random attack from 15 to 20
+        setAbility("FIRE BALL", 25);
+        setName(codeAMonNameBucket[rnd.nextInt(19)]); // select random name from 0 to 20
+        // System.out.println("\n>>> DEBUG: Created basic code-a-mon from FireMon\n");
+    }
 
-	public int attack(CodeAMon opponent, String weather) {
+    /**
+     * Method calculates the damage dealt by a CodeAMon when it attacks an opponent.
+     * The damage calculation includes the CodeAMon's attack value, the power of the
+     * "FIRE BALL" ability, and any weather or type bonuses or penalties.
+     * @param opponent The CodeAMon that is being attacked.
+     * @param weather The current weather condition in the battle.
+     * @return An integer representing the amount of damage dealt by the attacking CodeAMon.
+     */
+    public int attack(CodeAMon opponent, String weather) {
 
-		int abilityChance = rnd.nextInt(2);
-		int dealtDamage;
+        // int abilityChance = rnd.nextInt(2);
+        int dealtDamage = this.getAttack() + this.getAbilities().get("FIRE BALL");
 
-		if (abilityChance == 0) {
-			// chance that attacker attacked with basic attack and used an ability
-			dealtDamage = this.getAttack() + this.getAbilities().get("FIRE BALL");
-		} else {
-			// chance that attacker used only basic attack
-			dealtDamage = this.getAttack();
-		}
+        // bonus when weather is sunny
+        if (weather == "SUNNY") {
+            dealtDamage += 5;
+        }
 
-		// bonus when weather is sunny
-		if (weather == "SUNNY") {
-			dealtDamage += 5;
-		}
+        // bonus when opponent is nature
+        if (opponent.getType().equals("NATURE")) {
+            dealtDamage += 5;
+        }
 
-		// bonus when opponent is nature
-		if (opponent.getType().equals("NATURE")) {
-			dealtDamage += 5;
-		}
+        // penalty if opponent is water
+        if (opponent.getType().equals("WATER")) {
+            dealtDamage -= 5;
+        }
 
-		// penalty if opponent is water
-		if (opponent.getType().equals("WATER")) {
-			dealtDamage -= 5;
-		}
+        return dealtDamage;
+    }
 
-		return dealtDamage;
-	}
-
-	public int takeDamage(int damage) {
-		int newHP = this.getHP() - damage;
-		this.setHP(newHP);
-		return newHP;
-	}
+    /**
+     * Method calculates the amount of damage a CodeAMon
+     * receives from an attack and updates the CodeAMon's HP accordingly.
+     * @param damage An integer representing the amount of damage received by the CodeAMon.
+     * @return An integer representing the CodeAMon's remaining HP after the attack.
+     */
+    public int takeDamage(int damage) {
+        int newHp = this.getHp() - damage;
+        this.setHp(newHp);
+        return newHp;
+    }
 }
